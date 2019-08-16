bae2d99174498bc18cbd5e4d99eb2b1109dfac92
VeerPillai <52059338+VeerPillai@users.noreply.github.com>
2019-08-16 20:24:41 +0530

    Add files via upload

---
+++ timer.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+import datetime
+import math
+#datetime.datetime.now()= device time batayega
+#datetime.timedelta(seconds)=apan jo time diya vo
+#after == almost like sleep in multithreading
+class App():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.done_time=datetime.datetime.now() + datetime.timedelta(seconds=1200) # half hour
+        self.label = tk.Label(text="")
+        self.label.pack()
+        self.update_clk()
+        self.root.mainloop()
+
+    def update_clk(self):
+        elapsed = self.done_time - datetime.datetime.now()
+        h,m,s = elapsed.seconds/3600,elapsed.seconds/60,elapsed.seconds%60
+        fractional = math.floor(elapsed.microseconds/1000000.0*100)
+        self.label.configure(text="%02d:%02d:%02d.%02d"%(h,m,s,fractional),fg='red')
+        self.root.after(10, self.update_clk)
+
+app=App()
